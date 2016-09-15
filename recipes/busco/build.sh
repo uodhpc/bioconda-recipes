12dfea9a75cb3461fa829d3757dfa4056b42c100
Tom Walsh <walshtp@gmail.com>
2016-09-15 11:51:02 +0100

    Update busco recipe to version 1.22

--- recipes/busco/build.sh
+++ recipes/busco/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir -p $PREFIX/bin/
-cp BUSCO_v1.2.py $PREFIX/bin
-ln -s $PREFIX/bin/BUSCO_v1.2.py $PREFIX/bin/busco
+cp BUSCO_v1.22.py $PREFIX/bin
+ln -s $PREFIX/bin/BUSCO_v1.22.py $PREFIX/bin/busco
